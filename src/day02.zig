37cad171c5edeba5255927892673106eef1311ce
insolor <2442833+insolor@users.noreply.github.com>
2024-12-02 19:39:18 +0300

    day-02 part-2

--- src/day02.zig
+++ src/day02.zig
@@ -1,43 +1,88 @@
 const std = @import("std");
+const ArrayList = std.ArrayList;
 
 const input = @embedFile("inputs/02.txt");
 
+fn check_safe(row: []const i32, skip: ?usize) bool {
+    var prev: ?i32 = null;
+    var asc: ?bool = null;
+    for (row, 0..) |value, i| {
+        if (i == skip) continue;
+
+        if (prev == null) {
+            prev = value;
+            continue;
+        }
+
+        const new_asc = value > prev.?;
+
+        if (asc == null) {
+            asc = new_asc;
+        } else if (asc.? != new_asc) {
+            return false;
+        }
+
+        const delta = @abs(prev.? - value);
+        if (delta < 1 or delta > 3) {
+            return false;
+        }
+        prev = value;
+    }
+
+    return true;
+}
+
 pub fn run() !void {
     std.debug.print("Day 02\n", .{});
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+    defer _ = gpa.deinit();
+
+    var rows = ArrayList(ArrayList(i32)).init(allocator);
+    defer {
+        for (rows.items) |row| {
+            row.deinit();
+        }
+        rows.deinit();
+    }
 
     var lines = std.mem.split(u8, input, "\n");
-    var safe: u32 = 0;
 
     while (lines.next()) |line| {
         var parts = std.mem.split(u8, line, " ");
+        var row = ArrayList(i32).init(allocator);
 
-        var prev: ?i32 = null;
-        var asc: ?bool = null;
         while (parts.next()) |part| {
             const value = try std.fmt.parseInt(i32, part, 10);
+            try row.append(value);
+        }
 
-            if (prev == null) {
-                prev = value;
-                continue;
-            }
+        try rows.append(row);
+    }
 
-            const new_asc = value > prev.?;
-
-            if (asc == null) {
-                asc = new_asc;
-            } else if (asc.? != new_asc) {
-                break;
-            }
-
-            const delta = @abs(prev.? - value);
-            if (delta < 1 or delta > 3) {
-                break;
-            }
-            prev = value;
-        } else {
+    var safe: u32 = 0;
+    for (rows.items) |row| {
+        if (check_safe(row.items, null)) {
             safe += 1;
         }
     }
 
     std.debug.print("Part 1: {}\n", .{safe});
+
+    safe = 0;
+    for (rows.items) |row| {
+        if (check_safe(row.items, null)) {
+            safe += 1;
+            continue;
+        }
+
+        for (0..row.items.len) |i| {
+            if (check_safe(row.items, i)) {
+                safe += 1;
+                break;
+            }
+        }
+    }
+
+    std.debug.print("Part 2: {}\n", .{safe});
 }
