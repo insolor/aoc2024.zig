455b4bbcd65bdc69628b9d842ece5e32690362dd
insolor <2442833+insolor@users.noreply.github.com>
2024-12-02 18:02:52 +0300

    day-01

---
+++ src/day01.zig
@@ -0,0 +1,49 @@
+const std = @import("std");
+const ArrayList = std.ArrayList;
+const input = @embedFile("inputs/01.txt");
+
+pub fn run() !void {
+    std.debug.print("Day 01\n", .{});
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    const allocator = arena.allocator();
+    defer _ = arena.deinit();
+
+    var pairs = ArrayList(ArrayList(i32)).init(allocator);
+
+    var lines = std.mem.split(u8, input, "\n");
+    while (lines.next()) |line| {
+        var parts = std.mem.split(u8, line, " ");
+        var pair = ArrayList(i32).init(allocator);
+
+        while (parts.next()) |part| {
+            if (part.len == 0) {
+                continue;
+            }
+
+            try pair.append(try std.fmt.parseInt(i32, part, 10));
+        }
+
+        if (pair.items.len != 2) {
+            continue;
+        }
+        try pairs.append(pair);
+    }
+
+    var left = try allocator.alloc(i32, pairs.items.len);
+    var right = try allocator.alloc(i32, pairs.items.len);
+
+    for (pairs.items, 0..) |pair, i| {
+        left[i] = pair.items[0];
+        right[i] = pair.items[1];
+    }
+
+    std.mem.sort(i32, left, {}, comptime std.sort.asc(i32));
+    std.mem.sort(i32, right, {}, comptime std.sort.asc(i32));
+
+    var sum: u32 = 0;
+    for (left, right) |l, r| {
+        sum += @abs(l - r);
+    }
+
+    std.debug.print("Result: {}\n", .{sum});
+}
