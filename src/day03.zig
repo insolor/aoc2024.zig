cbbf5dc51e7b4ddd3dba4ac30bbfa73e9b440b67
insolor <2442833+insolor@users.noreply.github.com>
2024-12-03 22:40:05 +0300

    Move sum() parsing into a function

--- src/day03.zig
+++ src/day03.zig
@@ -1,6 +1,6 @@
 const std = @import("std");
 const input = @embedFile("inputs/03.txt");
-const prefix = "mul(";
+const mul_prefix = "mul(";
 
 fn getIntPart(s: []const u8) ?[]const u8 {
     for (s, 0..) |c, i| {
@@ -8,39 +8,51 @@ fn getIntPart(s: []const u8) ?[]const u8 {
             if (i == 0) {
                 return null;
             }
-            
+
             return s[0..i];
         }
     }
-    
+
     return null;
+}
+
+fn parseMul(s: []const u8) !?struct { value: u32, len: usize } {
+    var i: usize = 0;
+    if (!std.mem.eql(u8, s[0..mul_prefix.len], mul_prefix)) {
+        return null;
+    }
+
+    i += mul_prefix.len;
+    const arg1_slice = getIntPart(s[i..]) orelse return null;
+
+    if (s[i + arg1_slice.len] != ',') {
+        return null;
+    }
+
+    const arg1 = try std.fmt.parseInt(u32, arg1_slice, 10);
+
+    i += arg1_slice.len + 1;
+    const arg2_slice = getIntPart(s[i..]) orelse return null;
+    if (s[i + arg2_slice.len] != ')') {
+        return null;
+    }
+
+    const arg2 = try std.fmt.parseInt(u32, arg2_slice, 10);
+
+    return .{
+        .value = arg1 * arg2,
+        .len = i + arg2_slice.len,
+    };
 }
 
 fn part1() !void {
     var sum: u32 = 0;
 
-    for (0..input.len - prefix.len) |i| {
-        if (!std.mem.eql(u8, input[i .. i + prefix.len], prefix)) {
-            continue;
-        }
-
-        const arg1_start = i + prefix.len;
-        const arg1_slice = getIntPart(input[arg1_start ..]) orelse continue;
-        
-        if (input[arg1_start + arg1_slice.len] != ',') {
-            continue;
-        }
-        
-        const arg1 = try std.fmt.parseInt(u32, arg1_slice, 10);
-
-        const arg2_start: usize = arg1_start + arg1_slice.len + 1;
-        const arg2_slice = getIntPart(input[arg2_start ..]) orelse continue;
-        if (input[arg2_start + arg2_slice.len] != ')') {
-            continue;
-        }
-        const arg2 = try std.fmt.parseInt(u32, arg2_slice, 10);
-
-        sum += arg1 * arg2;
+    var i: usize = 0;
+    while (i < input.len - mul_prefix.len) : (i += 1) {
+        const result = try parseMul(input[i..]) orelse continue;
+        sum += result.value;
+        i += result.len;
     }
 
     std.debug.print("Part 1: {d}\n", .{sum});
