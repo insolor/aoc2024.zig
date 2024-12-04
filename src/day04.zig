a6638849224e6bc12aec46fdbf9bc973828b3585
insolor <2442833+insolor@users.noreply.github.com>
2024-12-04 23:29:43 +0300

    day04 part2

--- src/day04.zig
+++ src/day04.zig
@@ -117,7 +117,58 @@ fn part1() !void {
     std.debug.print("Part 1: {d}\n", .{count});
 }
 
-fn part2() !void {}
+fn checkXmas(data: []const []const u8, row: usize, column: usize) usize {
+    if (row + 2 >= data.len) {
+        return 0;
+    }
+
+    if (column + 2 >= data[row].len) {
+        return 0;
+    }
+
+    var diag: [3]u8 = undefined;
+    for (diag, 0..) |_, i| {
+        diag[i] = data[row + i][column + i];
+    }
+
+    if (!std.mem.eql(u8, &diag, "MAS") and !std.mem.eql(u8, &diag, "SAM")) {
+        return 0;
+    }
+
+    for (diag, 0..) |_, i| {
+        diag[i] = data[row + i][column + 2 - i];
+    }
+
+    if (!std.mem.eql(u8, &diag, "MAS") and !std.mem.eql(u8, &diag, "SAM")) {
+        return 0;
+    }
+
+    return 1;
+}
+
+fn part2() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+    defer _ = gpa.deinit();
+
+    const data = try loadData("src/inputs/04.txt", allocator);
+    defer {
+        for (data.items) |row| {
+            allocator.free(row);
+        }
+        data.deinit();
+    }
+
+    var count: usize = 0;
+    for (data.items, 0..) |row, i| {
+        for (row, 0..) |_, j| {
+            if (data.items[i][j] == 'M' or data.items[i][j] == 'S') {
+                count += checkXmas(data.items, i, j);
+            }
+        }
+    }
+    std.debug.print("Part 2: {d}\n", .{count});
+}
 
 pub fn run() !void {
     std.debug.print("Day 04\n", .{});
