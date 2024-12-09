cbad00921de8c0f5a0fdedac24b801da51b163a8
insolor <2442833+insolor@users.noreply.github.com>
2024-12-09 23:44:00 +0300

    part2 partially done (works only on test data)

--- src/day09.zig
+++ src/day09.zig
@@ -65,7 +65,9 @@ fn compactVer1(layout: *ArrayList(?usize), empty_spaces: *ArrayList(usize)) void
 fn calculateChecksum(layout: []const ?usize) usize {
     var checksum: usize = 0;
     for (layout, 0..) |value, i| {
-        checksum += value.? * i;
+        if (value != null) {
+            checksum += value.? * i;
+        }
     }
     return checksum;
 }
@@ -82,8 +84,109 @@ fn part1(disk_map: []const u8, allocator: std.mem.Allocator) !void {
     print("Part 1: {d}\n", .{calculateChecksum(layout.items)});
 }
 
-fn part2() !void {
-    print("Part 2\n", .{});
+const Block = struct {
+    index: usize,
+    size: usize,
+};
+
+fn getLastNotEmpty(layout: []const ?usize) ?Block {
+    const id = layout[layout.len - 1];
+    for (layout, 0..) |_, i| {
+        const block_start = layout.len - 1 - i;
+        if (layout[block_start] != id) {
+            return Block{ .index = block_start + 1, .size = layout.len - block_start - 1 };
+        }
+    }
+    return Block{ .index = 0, .size = layout.len - 1 };
+}
+
+fn printLayout(layout: []const ?usize) void {
+    for (layout) |value| {
+        if (value == null) {
+            print(".", .{});
+        } else {
+            print("{d}", .{value.?});
+        }
+    }
+    print("\n", .{});
+}
+
+fn compactVer2(layout: *ArrayList(?usize)) void {
+    var empty_blocks = ArrayList(Block).init(layout.allocator);
+    defer empty_blocks.deinit();
+
+    var block_start: ?usize = null;
+    for (layout.items, 0..) |value, i| {
+        if (block_start != null) {
+            if (value != null) {
+                empty_blocks.append(Block{ .index = block_start.?, .size = i - block_start.? }) catch unreachable;
+                block_start = null;
+            }
+        } else if (value == null) {
+            block_start = i;
+        }
+    }
+
+    // print("Empty blocks: {any}\n", .{empty_blocks.items});
+    // print("Layout: ", .{});
+    // printLayout(layout.items);
+
+    var i = layout.items.len;
+    var prev_id: ?usize = null;
+    while (i > 0) {
+        if (layout.items[i - 1] == null) {
+            i -= 1;
+            continue;
+        }
+
+        const last_block = getLastNotEmpty(layout.items[0..i]) orelse unreachable;
+
+        const id = layout.items[last_block.index].?;
+        defer prev_id = id;
+
+        if (id == 0) {
+            break;
+        }
+
+        if (prev_id != null and id >= prev_id.?) {
+            // print("Already seen\n", .{});
+        } else {
+            for (0..empty_blocks.items.len) |j| {
+                var empty = empty_blocks.items[j];
+                if (last_block.size <= empty.size) {
+                    // print("Block: {d} {any}\n", .{ id, last_block });
+                    std.debug.assert(layout.items[empty.index] == null);
+                    // print("Moved\n", .{});
+                    std.mem.copyBackwards(
+                        ?usize,
+                        layout.items[empty.index .. empty.index + last_block.size],
+                        layout.items[last_block.index .. last_block.index + last_block.size],
+                    );
+
+                    @memset(layout.items[last_block.index .. last_block.index + last_block.size], null);
+
+                    empty.size -= last_block.size;
+                    empty.index += last_block.size;
+                    empty_blocks.items[j] = empty;
+
+                    break;
+                }
+            }
+        }
+
+        i -= last_block.size;
+        // print("Layout: ", .{});
+        // printLayout(layout.items);
+    }
+}
+
+fn part2(disk_map: []const u8, allocator: std.mem.Allocator) !void {
+    var layout = getLayout(disk_map, allocator);
+    defer layout.deinit();
+
+    compactVer2(&layout);
+
+    print("Part 2: {d}\n", .{calculateChecksum(layout.items)});
 }
 
 pub fn main() !void {
@@ -97,7 +200,7 @@ pub fn main() !void {
     defer disk_map.deinit();
 
     try part1(disk_map.items, allocator);
-    try part2();
+    try part2(disk_map.items, allocator);
 
     print("\n", .{});
 }
