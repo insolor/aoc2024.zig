6fe1b8cf19aff431ee864e0fcf671bd3a2d8ced0
insolor <2442833+insolor@users.noreply.github.com>
2024-12-10 20:39:47 +0300

    day10 part2  (wait, WHY IT IS SO SIMPLE?)

--- src/day10.zig
+++ src/day10.zig
@@ -97,8 +97,36 @@ fn part1(data: []const []const u8, allocator: Allocator) !void {
     print("Part 1: {d}\n", .{total_score});
 }
 
-fn part2() !void {
-    print("Part 2\n", .{});
+fn findPath2(data: []const []const u8, x: usize, y: usize) usize {
+    if (data[y][x] == '9') {
+        return 1;
+    }
+
+    var score: usize = 0;
+    const current_value = data[@intCast(y)][@intCast(x)];
+    inline for (std.meta.fields(Direction)) |direction| {
+        const new_position = @field(Direction, direction.name)
+            .step(.{ .x = @intCast(x), .y = @intCast(y) });
+
+        if (!outOfBounds(new_position, data)) {
+            const new_value = data[@intCast(new_position.y)][@intCast(new_position.x)];
+            if (@as(i64, new_value) - current_value == 1) {
+                score += findPath2(data, @intCast(new_position.x), @intCast(new_position.y));
+            }
+        }
+    }
+    return score;
+}
+
+fn part2(data: []const []const u8) !void {
+    var iterator = TrailheadsIterator.init(data);
+
+    var total_score: usize = 0;
+    while (iterator.next()) |trailhead| {
+        total_score += findPath2(data, @intCast(trailhead.x), @intCast(trailhead.y));
+    }
+
+    print("Part 2: {d}\n", .{total_score});
 }
 
 pub fn main() !void {
@@ -110,6 +138,7 @@ pub fn main() !void {
     defer data.deinit();
 
     try part1(data.items, allocator);
+    try part2(data.items);
 
     print("Day 10\n", .{});
 }
