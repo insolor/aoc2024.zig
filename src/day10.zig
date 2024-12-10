ed064c0d921cb29d086ad67b3791110e038e7a7f
insolor <2442833+insolor@users.noreply.github.com>
2024-12-10 19:34:40 +0300

    Implement TrailheadsIterator

--- src/day10.zig
+++ src/day10.zig
@@ -5,7 +5,65 @@ const print = std.debug.print;
 
 const input = @embedFile("inputs/10.txt");
 
-fn part1() !void {
+fn loadData(allocator: std.mem.Allocator) !ArrayList([]const u8) {
+    var lines = std.mem.splitSequence(u8, input, "\n");
+
+    var result = ArrayList([]const u8).init(allocator);
+    while (lines.next()) |line| {
+        if (line.len == 0) {
+            continue;
+        }
+        try result.append(line);
+    }
+
+    return result;
+}
+
+const Position = struct { x: usize, y: usize };
+
+const TrailheadsIterator = struct {
+    field: []const []const u8,
+    x: usize,
+    y: usize,
+
+    fn init(field: []const []const u8) TrailheadsIterator {
+        return .{ .field = field, .x = 0, .y = 0 };
+    }
+
+    fn next(self: *TrailheadsIterator) ?Position {
+        if (self.y > self.field.len) {
+            return null;
+        }
+
+        while (true) {
+            var position: ?Position = null;
+            if (self.field[self.y][self.x] == '0') {
+                position = Position{ .x = self.x, .y = self.y };
+            }
+
+            self.x += 1;
+            if (self.x >= self.field[self.y].len) {
+                self.y += 1;
+                self.x = 0;
+            }
+
+            if (self.y >= self.field.len) {
+                return null;
+            }
+
+            if (position != null) {
+                return position;
+            }
+        }
+    }
+};
+
+fn part1(data: []const []const u8) !void {
+    var iterator = TrailheadsIterator.init(data);
+    while (iterator.next()) |trailhead| {
+        print("{any}, {c}\n", .{ trailhead, data[trailhead.y][trailhead.x] });
+    }
+
     print("Part 1\n", .{});
 }
 
@@ -14,5 +72,10 @@ fn part2() !void {
 }
 
 pub fn main() !void {
+    const data = try loadData(std.heap.page_allocator);
+    defer data.deinit();
+
+    try part1(data.items);
+
     print("Day 10\n", .{});
 }
